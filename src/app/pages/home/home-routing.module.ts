15623ec8ea30afcc9eeb49b9b53d58f1eb53c4bf
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2020-08-10 13:19:26 -0500

    Installing Angular Material and design of login

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: '/home/login',
     pathMatch: 'full'
   }
 ];

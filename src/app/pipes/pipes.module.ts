4de6d1f845de53a928f4bb1f22654045d1a89ca5
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2020-08-14 17:15:31 -0500

    Pipe for show category name added

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NumberPhonePipe } from './number-phone.pipe';
 import { SplitWordPipe } from './split-word.pipe';
+import { CategoryPipe } from './category.pipe';
 
 
 
 @NgModule({
-  declarations: [NumberPhonePipe, SplitWordPipe],
+  declarations: [NumberPhonePipe, SplitWordPipe, CategoryPipe],
   imports: [
     CommonModule
   ],

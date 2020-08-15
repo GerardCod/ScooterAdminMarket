bf9de0fc433b88d6a388bf1afc288f6491ff9f00
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2020-08-14 19:53:48 -0500

    Bug in delete functionality

--- src/app/pages/products/info-product/delete-product-dialog/delete-product-dialog.component.ts
+++ src/app/pages/products/info-product/delete-product-dialog/delete-product-dialog.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { Subscription } from 'rxjs';
+import { ProductsService } from 'src/app/services/products.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-product-dialog',
   templateUrl: './delete-product-dialog.component.html',
   styleUrls: ['./delete-product-dialog.component.scss']
 })
-export class DeleteProductDialogComponent implements OnInit {
+export class DeleteProductDialogComponent implements OnInit, OnDestroy {
+  product: Product;
+  deleteSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private productsService: ProductsService,
+    @Inject(MAT_DIALOG_DATA) data: Product,
+    private dialogRef: MatDialogRef<DeleteProductDialogComponent>,
+  ) {
+    this.product = data;
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    if (this.deleteSubscription) {
+      this.deleteSubscription.unsubscribe();
+    }
+  }
+
+  deleteProduct(): void {
+    this.deleteSubscription = this.productsService.deleteProduct(this.product)
+    .subscribe((data: any) => {
+      console.log(data);
+      this.dialogRef.close();
+    });
+  }
 }

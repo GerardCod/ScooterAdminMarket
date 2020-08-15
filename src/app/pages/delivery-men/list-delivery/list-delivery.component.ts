83e80e497c69f172b07c68d1226f841fd7fc27ec
Armando <armando14694@hotmail.com>
2020-08-15 16:35:01 -0500

    modify  in delivery

--- src/app/pages/delivery-men/list-delivery/list-delivery.component.ts
+++ src/app/pages/delivery-men/list-delivery/list-delivery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AddDeliveryComponent } from '../add-delivery/add-delivery.component';
 import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 
 
 @Component({
@@ -9,6 +10,13 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./list-delivery.component.scss']
 })
 export class ListDeliveryComponent implements OnInit {
+  // MatPaginator Inputs
+  length = 100;
+  pageSize = 25;
+  pageSizeOptions: number[] = [25, 50, 75, 100];
+
+  params = { limit: 15, offset: 0, search: '', ordering: '', status: 1 };
+  deliverys: Array<any> = [];
 
   loadingDelivery: boolean;
 
@@ -17,6 +25,7 @@ export class ListDeliveryComponent implements OnInit {
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
 
   dialogAddDelivery() {
@@ -27,5 +36,20 @@ export class ListDeliveryComponent implements OnInit {
       data: { delivery: null }
     });
   }
+
+
+  // Metodo paginator
+  getPages(e): PageEvent {
+    if (this.deliverys.length === 0) {
+      this.pageSize = 25;
+      return;
+    }
+    this.params.limit = e.pageSize;
+    this.params.offset = this.params.limit * e.pageIndex;
+    // this.getDelivery();
+  }
+  // ==========================================
+
+
 
 }
